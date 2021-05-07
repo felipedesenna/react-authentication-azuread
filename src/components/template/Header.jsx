002f37fdd05fd7f648346487d76e4b5077296051
rodsl <rodsl@icloud.com>
2021-05-07 17:02:04 -0300

    Commit 07/05/2021

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { useAuth } from '../../hooks/auth';
+import { useAuth } from "../../hooks/auth";
 
-import logo from '../../assets/logo.svg';
-import '../../assets/styles.css';
+import logo from "../../assets/logo.svg";
+import "../../assets/styles.css";
 
 const Dashboard = (props) => {
-    const { signOut, accountInfo } = useAuth();
+  const { signOut, accountInfo } = useAuth();
 
-    return (
+  return (
+    <header className="header d-none d-sm-flex flex-column">
+      <h1 className="mt-3">
+        {props.icon} {props.title}
+        <div className="accountinfo">
+          <p>
+            Bem-vindo,
+            <strong> {accountInfo.user.displayName}</strong>
+            <button
+              type="button"
+              className="botao App-button "
+              onClick={signOut}
+            >
+              sair
+            </button>
+          </p>
+        </div>
+      </h1>
 
-        <header className="header d-none d-sm-flex flex-column">
-            <h1 className="mt-3">
-                {props.icon} {props.title}
-                <div className="accountinfo">
-                    <p>Bem-vindo,
-                        <strong> {accountInfo.user.displayName}</strong>
-                    <button type="button" className="botao App-button " onClick={signOut}>sair</button>
-                    </p>
-                </div>
-            </h1>
-
-
-            <p className="lead text-muted">{props.subtitle}</p>
-        </header>
-
-    );
+      <p className="lead text-muted">{props.subtitle}</p>
+    </header>
+  );
 };
 
 export default Dashboard;
